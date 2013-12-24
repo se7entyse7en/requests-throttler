0ce62941ad13a32402c6b15ddac304663839ea22
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2013-12-24 15:54:11 +0100

    Added logging settings

--- settings.py
+++ settings.py
@@ -1,3 +1,19 @@
+import logging
+
 VERSION=0.1
 AUTHOR='Lou Marvin Caraig as se7entyse7en'
 CONTRIBUTORS=""
+
+###----------------------###
+#---- Logging settings ----#
+###----------------------###
+LOG_FORMAT_0=("[TID=%(thread)d - Thread=%(threadName)s - %(asctime)s - %(levelname)s - "
+              "%(module)s.%(funcName)s(%(lineno)d)]: %(message)s")
+LOG_FORMAT_1=("[Thread=%(threadName)s - %(asctime)s - %(levelname)s]: %(message)s")
+LOG_FORMAT_2=("[%(asctime)s - %(levelname)s]: %(message)s")
+LOG_FORMAT={logging.DEBUG: LOG_FORMAT_0,
+            logging.INFO: LOG_FORMAT_1,
+            logging.WARNING: LOG_FORMAT_2,
+            logging.ERROR: LOG_FORMAT_0,
+            logging.CRITICAL: LOG_FORMAT_0}
+DEFAULT_LOG_LEVEL=logging.INFO
