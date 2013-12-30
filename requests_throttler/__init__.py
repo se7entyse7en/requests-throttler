de8a4b77bf966e2455102b20ee6318bb4b1d88a0
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2013-12-31 00:13:17 +0100

    Fixed imports in main __init__

--- requests_throttler/__init__.py
+++ requests_throttler/__init__.py
@@ -5,3 +5,8 @@ __author_email__ = 'loumarvincaraig@gmail.com'
 __project_url__ = 'https://github.com/se7entyse7en/requests-throttler'
 __license__ = 'Apache 2.0'
 __copyright__ = 'Copyright 2013 Lou Marvin Caraig'
+
+
+from . import utils
+from .throttled_request.throttled_request import ThrottledRequest
+from .throttler.base_throttler import BaseThrottler
