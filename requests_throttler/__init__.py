bfe0ecce803a7e0af1af4df5a229db256c0bfba2
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2014-01-04 14:35:16 +0100

    Modified CHANGES.rst, HISTORY.rst, main __init__.py

--- requests_throttler/__init__.py
+++ requests_throttler/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'RequestsThrottler'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 __author__ = 'Lou Marvin Caraig'
 __author_email__ = 'loumarvincaraig@gmail.com'
 __project_url__ = 'https://github.com/se7entyse7en/requests-throttler'
@@ -10,3 +10,4 @@ __copyright__ = 'Copyright 2013 Lou Marvin Caraig'
 from . import utils
 from .throttled_request import ThrottledRequest
 from .throttler import BaseThrottler
+from .exceptions import *
