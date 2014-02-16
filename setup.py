7f6964cdb8c619170782c8ca6dd0dca5b22a5f63
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2014-02-16 20:10:27 +0100

    Fixed installation via pip

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
+import re
 from setuptools import setup
 
-import requests_throttler
+TITLE_REGEX = r"^__title__\s=\s(?P<quote>['])(?P<title>\w*)(?P=quote)$"
+VERSION_REGEX = r"^__version__\s=\s(?P<quote>['])(?P<version>[\d\.]*)(?P=quote)$"
+LICENSE_REGEX = r"^__license__\s=\s(?P<quote>['])(?P<license>.*)(?P=quote)$"
+PROJECT_URL_REGEX = r"^__project_url__\s=\s(?P<quote>['])(?P<project_url>.*)(?P=quote)$"
+AUTHOR_REGEX = r"^__author__\s=\s(?P<quote>['])(?P<author>[\w\s]*)(?P=quote)$"
+AUTHOR_EMAIL_REGEX = r"^__author_email__\s=\s(?P<quote>['])(?P<author_email>.*)(?P=quote)$"
 
 
 packages = ['requests_throttler',
@@ -25,15 +31,24 @@ with open('README.rst') as f:
     readme = f.read()
 with open('HISTORY.rst') as f:
     history = f.read()
+with open('./requests_throttler/__init__.py') as f:
+    datas = f.read()
 
-setup(name=requests_throttler.__title__,
-      version=requests_throttler.__version__,
-      license=requests_throttler.__license__,
-      url=requests_throttler.__project_url__,
+title = re.search(TITLE_REGEX, datas, re.MULTILINE).group('title')
+version = re.search(VERSION_REGEX, datas, re.MULTILINE).group('version')
+license = re.search(LICENSE_REGEX, datas, re.MULTILINE).group('license')
+project_url = re.search(PROJECT_URL_REGEX, datas, re.MULTILINE).group('project_url')
+author = re.search(AUTHOR_REGEX, datas, re.MULTILINE).group('author')
+author_email = re.search(AUTHOR_EMAIL_REGEX, datas, re.MULTILINE).group('author_email')
+
+setup(name=title,
+      version=version,
+      license=license,
+      url=project_url,
       description='Python HTTP requests throttler',
       long_description=readme + '\n\n' + history,
-      author=requests_throttler.__author__,
-      author_email=requests_throttler.__author_email__,
+      author=author,
+      author_email=author_email,
       packages=packages,
       package_dir={'requests_throttler': 'requests_throttler'},
       package_data={'': ['LICENSE']},
